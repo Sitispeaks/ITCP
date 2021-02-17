9cfae47f2e9bd09f1d32b3ffe9470bf57214a321
Sitikanta-Panigrahi <60188244+Sitispeaks@users.noreply.github.com>
2021-02-17 13:42:35 +0530

    changed req.txt and settings.py

--- resumeproject/settings.py
+++ resumeproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY ='!(h)8w@qrj(!g6pk=)xqd^7f#=iqi5&s6kyl8ev7+vsi0cg6+v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['itcareerpoint.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # mail config
