c07b6069db0afdd0d7e6ff3026ae6d760af1a642
Sitikanta-Panigrahi <60188244+Sitispeaks@users.noreply.github.com>
2021-02-17 13:48:41 +0530

    changed req.txt and settings.py

--- resumeproject/settings.py
+++ resumeproject/settings.py
@@ -166,3 +166,4 @@ django_heroku.settings(locals())
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # AWS_S3_HOST = "s3.us-east-2.amazonaws.com" 
 # AWS_S3_REGION_NAME = "us-east-2"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
